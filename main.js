d340a1d43c8df3e53f6753ee4e307709f4b67d1f
Luan E <luan.erazo@skyrocketlab.space>
2023-12-17 01:52:57 -0500

    tailwind installed, and notion, first log of fetch info in notion failed

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { Client } from '@notionhq/client';
+// const { Client } = require("@notionhq/client")
+// Initializing a client
+const notion = new Client({
+  auth: import.meta.env.VITE_NOTION_KEY,
+});
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+(async () => {
+  const blockId = import.meta.env.VITE_NOTION_PAGE_ID;
+  const response = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50,
+  });
+  console.log(response);
+})();
